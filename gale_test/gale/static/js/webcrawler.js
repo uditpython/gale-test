eeb4f3c1cf2d1785a04ed28009736ee5be1f3eba
uditmital1986 <udit.mital@aggrigator.com>
2018-04-03 15:54:32 +0530

    make change in javscript for alert messages and optimize javascript

--- gale_test/gale/static/js/webcrawler.js
+++ gale_test/gale/static/js/webcrawler.js
@@ -3,6 +3,30 @@ app.controller('webcrawlerController', function($scope, $http) {
 
         
         $scope.tocrawl = function(url,depth){
+        
+        if (url === ''){
+             window.alert("Empty URL");
+             return;
+        }
+        
+        if (typeof(url) === 'undefined'){
+             window.alert("Empty URL");
+             return;
+        }
+        
+        if (typeof(depth) === 'undefined'){
+             window.alert("Empty depth");
+             return;
+        }
+        if ( depth === null){
+             window.alert("Empty Depth");
+             return;
+        }
+        
+        if (!/^(f|ht)tps?:\/\//i.test(url)) {
+            url = "http://" + url;
+            }
+              
         var data = {url:url,depth:depth};
         var back_url  = '/web_crawler/';
  
@@ -12,13 +36,14 @@ app.controller('webcrawlerController', function($scope, $http) {
           data: JSON
       }).success(function(data, status, headers, config) {
             $scope.final_data = data;
+            return;
         }
         )
         
         .error(function(data, status, headers, config) {
           
           window.alert("Wrong URL");
-          
+          return;
         });
     
         
--- gale_test/gale/views.py
+++ gale_test/gale/views.py
@@ -1,21 +1,46 @@
 # -*- coding: utf-8 -*-
-import threading
 from __future__ import unicode_literals
+import threading
 import requests
 import time
 from django.shortcuts import render
 from django.http.response import HttpResponse
 import json
+from urlparse import urlparse
 from bs4 import BeautifulSoup
-from settings import NUMBER_OF_THREADS
+from gale_test.settings import NUMBER_OF_THREADS
+import Queue
+queue=Queue.Queue()
+
+class Crawler:
+
+    
+
+    def __init__(self, link_list = [], images_list=[],explored_list = []):
+        Crawler.link_list = link_list
+        Crawler.images_list = images_list
+        Crawler.explored_list = explored_list
+
+crawler = Crawler() 
+
+
+
 
 # Create your views here.
 def home(request):
     template = 'gale/index.html'
     return render(request,template)
 
-def get_data(url,session):
-    url_data = session.get(url)
+def get_data(url,session=None):
+    
+    
+    if session == None:
+        session = requests.session()
+    try:
+        url_data = session.get(url)
+        crawler.explored_list.append(url)
+    except:
+        return None 
     
     soup = BeautifulSoup(url_data.text,"html.parser")
     s1 = soup.find_all('a', href=True)
@@ -30,7 +55,8 @@ def get_data(url,session):
             else:
                 final_link = link['href']
             
-            links_to_be_shown.append(final_link)   
+            if urlparse(final_link).scheme in ['http','https'] :
+                links_to_be_shown.append(final_link)   
     
     tags_shown = []
     tags=soup.findAll('img')
@@ -59,7 +85,7 @@ def get_data(url,session):
     data = {}
     data['links'] = links_to_be_shown
     data['images'] = tags_shown
-
+    
     return data
     
     
@@ -73,6 +99,7 @@ def validate_images(image_list,session):
         return valid_images
     
 def validate_urls(url_list,session):
+        
         valid_urls = []
         for i in url_list:
             try:
@@ -84,29 +111,51 @@ def validate_urls(url_list,session):
     
     
     
-
-
-
-
     
+# Create worker threads (will die when main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do the next job in the queue
+def work():
+    cd = []
+    while True:
+        url = queue.get()
         
+        data_new = get_data(url)
+        if data_new != None:
+            crawler.images_list += data_new['images']
+            crawler.link_list += data_new['links'] 
+        queue.task_done()
+
+
+# Each queued link is a new job
+def create_jobs(seed_urls_to_be_expolred):
+    for link in seed_urls_to_be_expolred:
+        queue.put(link)
+    queue.join()
+    
+
+
+# Check if there are items in the queue, if so crawl them
+def crawl(seed_urls_to_be_expolred):
+    queued_links = seed_urls_to_be_expolred
+    if len(queued_links) > 0:
+        
+        create_jobs(seed_urls_to_be_expolred)
 
 
 
 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 
 def web_crawler(request):
+    
     url = request.GET.get('url',None)
     
     depth = int(request.GET.get('depth',None))
@@ -117,27 +166,36 @@ def web_crawler(request):
     seed_urls_explored = []
     images_list = data['images']
     if depth == 0:
-        seed_urls_explored = validate_urls(seed_urls_to_be_expolred)
+        
+        seed_urls_explored = seed_urls_to_be_expolred
         
     else:
         depth_initial = 1
         
         
         while depth_initial <= depth:
-            create_workers()
+            
             temp_urls = []
+            crawler.images_list = []
+            crawler.link_list = []
+            
             if len(seed_urls_to_be_expolred) > 0:
-                create_jobs()
-                for (i,j) in enumerate(seed_urls_to_be_expolred):
-                    if j not in seed_urls_explored:
-                        try:
-                            new_data = get_data(j,session)
-                            
-                            temp_urls  +=  new_data['links']
-                            images_list +=  new_data['images']
-                            seed_urls_explored.append(j)
-                        except Exception as e:
-                            pass
+                
+                create_workers()
+                crawl(seed_urls_to_be_expolred)
+                temp_urls += crawler.link_list
+                images_list +=  crawler.images_list
+                
+#                 for (i,j) in enumerate(seed_urls_to_be_expolred):
+#                     if j not in seed_urls_explored:
+#                         try:
+#                             new_data = get_data(j,session)
+#                             
+#                             temp_urls  +=  new_data['links']
+#                             images_list +=  new_data['images']
+#                             seed_urls_explored.append(j)
+#                         except Exception as e:
+#                             pass
                     
              
             seed_urls_to_be_expolred = list(set(temp_urls))
@@ -146,11 +204,10 @@ def web_crawler(request):
             depth_initial += 1    
         
         if len(seed_urls_to_be_expolred) > 0:
-            seed_urls_explored += validate_urls(seed_urls_to_be_expolred,session)
-    
+            seed_urls_explored = seed_urls_to_be_expolred + crawler.explored_list   
     final_data = {}
     final_data['links'] = seed_urls_explored
-    final_data['images'] = validate_images(list(set(images_list)),session)
+    final_data['images'] = list(set(images_list))
         
     return HttpResponse(json.dumps(final_data) , content_type="application/json")
     
