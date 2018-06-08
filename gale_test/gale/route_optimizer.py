e31acdbe7902b683cdf4bd25ede4aa57996c38eb
Udit Mital <Udit Mital@DESKTOP-DFS2OR8>
2018-06-08 09:43:32 +0530

    number of threads 4

--- gale_test/gale/route_optimizer.py
+++ gale_test/gale/route_optimizer.py
@@ -76,7 +76,7 @@ def distance1(input):
     return float(dist)
 
 def create_workers():
-    for _ in range(2):
+    for _ in range(4):
         t = threading.Thread(target=work)
         t.daemon = True
         t.start()
