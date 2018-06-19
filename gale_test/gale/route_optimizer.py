d836aba715608d33606f870b3348480c91141bef
Udit Mital <Udit Mital@DESKTOP-DFS2OR8>
2018-06-19 08:42:09 +0530

    remove try exception for osrm

--- gale_test/gale/route_optimizer.py
+++ gale_test/gale/route_optimizer.py
@@ -89,7 +89,7 @@ def distance1(input):
     return float(dist)
 
 def create_workers():
-    for _ in range(16):
+    for _ in range(100):
         t = threading.Thread(target=work)
         t.daemon = True
         t.start()
@@ -97,18 +97,13 @@ def create_workers():
 
 
 def work_temp(cordinates):
+    
     try:
         matrix[cordinates[1]][cordinates[2]] = matrix[cordinates[2]][cordinates[1]]  
         
     except:
-        try:
-            matrix[cordinates[1]][cordinates[2]] = distance1(cordinates[3:])
-        except:
-            try:
-                matrix[cordinates[1]][cordinates[2]] = distance1(cordinates[3:])
-            except:
-                matrix[cordinates[1]][cordinates[2]] = distance1(cordinates[3:])
-
+        
+        matrix[cordinates[1]][cordinates[2]] = distance_osrm(cordinates[3:])
 
 
 def work():
@@ -156,7 +151,7 @@ class CreateDistanceCallback(object):
     
     from multiprocessing import Pool
     cd = []
-    create_workers()
+#     create_workers()
     num_locations = len(locations)
     self.matrix = {}
     
@@ -172,12 +167,12 @@ class CreateDistanceCallback(object):
         y2 = locations[to_node][1]
         cd.append([from_node,to_node,x1,y1,x2,y2])
         
-#         work_temp([len(cd),from_node,to_node,x1,y1,x2,y2])
+        work_temp([len(cd),from_node,to_node,x1,y1,x2,y2])
 #         if distance1([x1,y1,x2,y2]) > 40:
 #             print x1,y1,x2,y2,distance1([x1,y1,x2,y2])
 #         
-        queue.put([len(cd),from_node,to_node,x1,y1,x2,y2])
-    queue.join()   
+#         queue.put([len(cd),from_node,to_node,x1,y1,x2,y2])
+#     queue.join()   
 #           
      
 #         self.matrix[from_node][to_node] = distance1([x1,y1,x2,y2])
