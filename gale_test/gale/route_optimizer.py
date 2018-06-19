3b3f2e522fb50abe7cfeb4b507b1c117c9d27f95
Udit Mital <Udit Mital@DESKTOP-DFS2OR8>
2018-06-19 15:00:39 +0530

    removed osrm reference

--- gale_test/gale/route_optimizer.py
+++ gale_test/gale/route_optimizer.py
@@ -151,7 +151,7 @@ class CreateDistanceCallback(object):
         
         except:
         
-            self.matrix[cordinates[1]][cordinates[2]] = distance_osrm(cordinates[3:])
+            self.matrix[cordinates[1]][cordinates[2]] = distance1(cordinates[3:])
 
     import  datetime    
     
@@ -174,12 +174,13 @@ class CreateDistanceCallback(object):
         x2 = locations[to_node][0]
         y2 = locations[to_node][1]
         cordinates = [1,from_node,to_node,x1,y1,x2,y2]
-        cd.append(cordinates)
-    pool = ThreadPool(4) 
-    results = pool.map(matrix, cd)
-    pool.close() 
-    pool.join()
-     
+        matrix(cordinates)
+#         cd.append(cordinates)
+#     pool = ThreadPool(4) 
+#     results = pool.map(matrix, cd)
+#     pool.close() 
+#     pool.join()
+#      
 #         try:
 #             self.matrix[cordinates[1]][cordinates[2]] = self.matrix[cordinates[2]][cordinates[1]]  
 #         
