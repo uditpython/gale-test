fa3b8dcd031df24ef8f897fa7ddc4af76fddf7b9
Udit Mital <Udit Mital@DESKTOP-DFS2OR8>
2018-06-19 10:48:59 +0530

    removed multi threading

--- gale_test/gale/route_optimizer.py
+++ gale_test/gale/route_optimizer.py
@@ -143,10 +143,10 @@ class CreateDistanceCallback(object):
   def __init__(self, locations):
     """Initialize distance array."""
     import  datetime    
-    
+    matrix = {}
     from multiprocessing import Pool
     cd = []
-    create_workers()
+#     create_workers()
     num_locations = len(locations)
     self.matrix = {}
     
@@ -162,12 +162,12 @@ class CreateDistanceCallback(object):
         y2 = locations[to_node][1]
         cd.append([from_node,to_node,x1,y1,x2,y2])
         
-#         work_temp([len(cd),from_node,to_node,x1,y1,x2,y2])
+        work_temp([len(cd),from_node,to_node,x1,y1,x2,y2])
 #         if distance1([x1,y1,x2,y2]) > 40:
 #             print x1,y1,x2,y2,distance1([x1,y1,x2,y2])
 #         
-        queue.put([len(cd),from_node,to_node,x1,y1,x2,y2])
-    queue.join()   
+#         queue.put([len(cd),from_node,to_node,x1,y1,x2,y2])
+#     queue.join()   
 #           
      
 #         self.matrix[from_node][to_node] = distance1([x1,y1,x2,y2])
